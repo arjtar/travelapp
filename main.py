4392c35676618e7a0e4dd536f1851506ae661322
github_arjtar <arjantpaudel@gmail.com>
2022-03-20 18:09:59 +0545

    learned

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
 	return render_template('index.html')
 
-@app. route('/register')
+@app.route('/register')
 def register():
 	return render_template('register.html')
 
-@app. route('/login')
+@app.route('/login')
 def login():
-
 	return render_template('login.html')
 
+	@app.route('/doLogin',methods=['POST'])
+	def doLogin();
+		email = request.form['email']
+		email = request.form['psw']
+
+		return render_template('home.html',result={"email":email}
 app.run()
